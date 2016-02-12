aa1f762ceb9ebe60e26156668c140ef8118261c2
mochablendy <mochablendy@gmail.com>
2016-02-12 13:48:54 +0900

    Add a main form window.

---
+++ G6i/FormMain.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace G6i
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxSetupFile = new System.Windows.Forms.TextBox();
+            this.tbxMotecFile = new System.Windows.Forms.TextBox();
+            this.btnSetupFile = new System.Windows.Forms.Button();
+            this.btnMotecFile = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnInject = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 27.00535F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 72.99465F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 88F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tbxSetupFile, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tbxMotecFile, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnSetupFile, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnMotecFile, 2, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(414, 100);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Setup File:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Motec File:";
+            // 
+            // tbxSetupFile
+            // 
+            this.tbxSetupFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbxSetupFile.Location = new System.Drawing.Point(91, 15);
+            this.tbxSetupFile.Name = "tbxSetupFile";
+            this.tbxSetupFile.Size = new System.Drawing.Size(231, 19);
+            this.tbxSetupFile.TabIndex = 2;
+            // 
+            // tbxMotecFile
+            // 
+            this.tbxMotecFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbxMotecFile.Location = new System.Drawing.Point(91, 65);
+            this.tbxMotecFile.Name = "tbxMotecFile";
+            this.tbxMotecFile.Size = new System.Drawing.Size(231, 19);
+            this.tbxMotecFile.TabIndex = 3;
+            // 
+            // btnSetupFile
+            // 
+            this.btnSetupFile.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnSetupFile.Location = new System.Drawing.Point(336, 13);
+            this.btnSetupFile.Name = "btnSetupFile";
+            this.btnSetupFile.Size = new System.Drawing.Size(75, 23);
+            this.btnSetupFile.TabIndex = 4;
+            this.btnSetupFile.Text = "Select...";
+            this.btnSetupFile.UseVisualStyleBackColor = true;
+            this.btnSetupFile.Click += new System.EventHandler(this.btnSetupFile_Click);
+            // 
+            // btnMotecFile
+            // 
+            this.btnMotecFile.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnMotecFile.Location = new System.Drawing.Point(336, 63);
+            this.btnMotecFile.Name = "btnMotecFile";
+            this.btnMotecFile.Size = new System.Drawing.Size(75, 23);
+            this.btnMotecFile.TabIndex = 5;
+            this.btnMotecFile.Text = "Select...";
+            this.btnMotecFile.UseVisualStyleBackColor = true;
+            this.btnMotecFile.Click += new System.EventHandler(this.btnMotecFile_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel1.Controls.Add(this.btnClose);
+            this.flowLayoutPanel1.Controls.Add(this.btnInject);
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(199, 137);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(227, 40);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.Location = new System.Drawing.Point(149, 11);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "&Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnInject
+            // 
+            this.btnInject.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnInject.Location = new System.Drawing.Point(39, 3);
+            this.btnInject.Name = "btnInject";
+            this.btnInject.Size = new System.Drawing.Size(104, 31);
+            this.btnInject.TabIndex = 1;
+            this.btnInject.Text = "&Inject";
+            this.btnInject.UseVisualStyleBackColor = true;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 189);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.Text = "G6i";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxSetupFile;
+        private System.Windows.Forms.TextBox tbxMotecFile;
+        private System.Windows.Forms.Button btnSetupFile;
+        private System.Windows.Forms.Button btnMotecFile;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnInject;
+    }
+}
+
---
+++ G6i/FormMain.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace G6i
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private string ChooseFile(CommonFileDialogFilter filter)
+        {
+            var dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = false;
+            dialog.EnsureReadOnly = false;
+            dialog.AllowNonFileSystemItems = false;
+            dialog.Filters.Add(filter);
+
+            var result = dialog.ShowDialog();
+
+            return result == CommonFileDialogResult.Ok ? dialog.FileName : null;
+        }
+
+        private void btnSetupFile_Click(object sender, EventArgs e)
+        {
+            var file = this.ChooseFile(new CommonFileDialogFilter("Setup", ".htm"));
+            if (file != null)
+            {
+                tbxSetupFile.Text = file;
+                tbxSetupFile.Select(tbxSetupFile.Text.Length, 0);
+            }
+        }
+
+        private void btnMotecFile_Click(object sender, EventArgs e)
+        {
+            var file = this.ChooseFile(new CommonFileDialogFilter("Motec", ".ldx"));
+            if (file != null)
+            {
+                tbxMotecFile.Text = file;
+                tbxMotecFile.Select(tbxMotecFile.Text.Length, 0);
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
