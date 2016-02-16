06c3b2783a4538fc4270f5a0df3f3256c11ba8d9
mochablendy <mochablendy@gmail.com>
2016-02-16 12:03:45 +0900

    Add a new test to the MotecFile.

--- TestG6i/MotecFile.cs
+++ TestG6i/MotecFile.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Xml.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using G6i;
 
@@ -11,7 +12,9 @@ namespace TestG6i
         [TestMethod]
         public void ReturnConstantValuesNodeProperly()
         {
-            var motec = MotecFile.Load(Path.GetFullPath(@"../../sample/sample-already.ldx");
+            var motec = MotecFile.Load(Path.GetFullPath(@"../../sample/sample-alreadyset.ldx"));
+            XElement element = motec.AsDynamic().ConstantValuesRoot();
+            element.Name.Is("Details");
         }
     }
 }
